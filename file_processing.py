04f9aa768ae3fe63102e0e7d9f38593dac637bb4
m.salewski <matthewsalewski@yahoo.com>
2020-04-21 17:44:09 +0200

    Add argparser args to main: dir, master.csv, verbose; unified format to saved htmls and all dirs; remove redundant file renaming functionality

--- file_processing.py
+++ file_processing.py
@@ -59,10 +59,25 @@ def get_source_dir(filename, directory, verbose=False):
     return source_fpath, change_dirname
         
 
+#strings_to_check_for = ["|", "(", ")", ".html.", ".m.b.H.", "html.html", "html_files", "files_files"]
+strings_to_check_for = ["|", "(", ")","-", ",", ".", "html"]
+def remove_substrings(checklist):
+    
+    def inner(string_to_check):
+            
+            output_string = string_to_check
+            
+            while any([True for s in checklist if s in output_string]):            
+                for s in checklist:
+                    output_string = output_string.replace(s,"")
+            return output_string
+        
+    return inner
+
 def rename_files_and_dirs(files, directory = './saved_webpages/', verbose=False):
 
     #main_dir = os.path.abspath(directory)
-
+    '''
     if 'Closed/' in directory:
         directory = directory.replace('Closed/','')
     elif 'Closed' in directory:
@@ -73,34 +88,90 @@ def rename_files_and_dirs(files, directory = './saved_webpages/', verbose=False)
         directory = directory.replace('Applied','')
     else:
         pass
+    '''
     dirs = directory + 'dirs/'
     
     for file_ in files:
 
         # Check if the file is already processed
         #if file_.split('_LinkedIn')[1] != '.html':
-        if len(file_.split('_LinkedIn')) > 1:            
-            if verbose: print('\t\tAlready processed:',file_)
-            continue
+        #if len(file_.split('_LinkedIn')) > 1:            
+        if any([True for s in [' ']+strings_to_check_for[:-2] if s in file_]):
+            if verbose: print('Processing:',file_)
         else:
-            if verbose: print(file_)
+            if verbose: print('\t\tAlready processed:',file_)
+            continue            
 
+        
         # Get job ID
         filename = directory+file_
         job_id = get_job_id_wrapper(filename)
     
-        newname = file_.replace(' ', '_').replace('LinkedIn.html', f'LinkedIn_{job_id}')
+        newname = remove_substrings(strings_to_check_for)(file_.replace(" ","_"))
+        newname = remove_substrings(["html"])(newname)
+        newname = newname+f"_{job_id}"
+            
+        #source_dpath, change_dirname = get_source_dir(file_, directory, verbose)
+        
+        source_fpath = os.path.join(directory,file_)
+        dest_fpath = os.path.join(directory,newname+'.html')
+        
+        if os.path.isfile(source_fpath):
+            os.rename(source_fpath,dest_fpath)
+        
+        source_dpath = os.path.join(directory,file_.replace('.html', "_files"))
+        dest_dpath = os.path.join(dirs,newname+'_files')
+        
+        if os.path.isdir(source_dpath):
+            os.rename(source_dpath,dest_dpath)
+
+    return None
+
+def rename_remove_vert(files, directory = './saved_webpages/', verbose=False):
+
+    #main_dir = os.path.abspath(directory)
+    '''
+    if 'Closed/' in directory:
+        directory = directory.replace('Closed/','')
+    elif 'Closed' in directory:
+        directory = directory.replace('Closed','')
+    elif 'Applied/' in directory:
+        directory = directory.replace('Applied/','')
+    elif 'Applied' in directory:
+        directory = directory.replace('Applied','')
+    else:
+        pass
+    '''
+    dirs = directory + 'dirs/'
+    
+    for file_ in files:
+
+        # Check if the file is already processed
+        #if file_.split('_LinkedIn')[1] != '.html':
+        if ("|" in file_) | ("(" in file_) | (")" in file_):
+            if verbose: print('Processing:',file_)
+        else:
+            if verbose: print('\t\tAlready processed:',file_)
+            continue
+        
+        # Get job ID
+        filename = directory+file_
+        job_id = get_job_id_wrapper(filename)
+    
+        newname = file_.replace('.html', '').replace('|', '').replace('(', '').replace(')', '')
             
         source_dpath, change_dirname = get_source_dir(file_, directory, verbose)
+        
         
         source_fpath = os.path.join(directory,file_)
         dest_fpath = os.path.join(directory,newname+'.html')
         os.rename(source_fpath,dest_fpath)
         
-        if (change_dirname):
-            source_dpath = os.path.join(source_dpath,filename.replace('.html', "_files"))
-            dest_dpath = os.path.join(dirs,newname+'_files')
-            
-            os.rename(source_dpath,dest_dpath)
+        
+        #if (change_dirname):
+        source_dpath = os.path.join(dirs,file_.replace('.html', "_files"))
+        dest_dpath = os.path.join(dirs,newname+'_files')
+        print(dest_dpath)
+        os.rename(source_dpath,dest_dpath)
 
     return None
--- loli_scraping.py
+++ loli_scraping.py
@@ -6,14 +6,35 @@ from bs4 import BeautifulSoup
 from pandas import DataFrame
 import re
 import datetime
-import sys
-import os
+import argparse
+import os, sys
 import pandas as pd
 
 from internal_processing import get_job_details, get_name_and_loc, get_posted_and_applicants
 from internal_processing import get_job_title, get_job_id, get_job_description
 from file_processing import get_files, rename_files_and_dirs
 from helpers import job_detail_keys, get_job_details_dc, clean_dict, get_deutsch, get_adj_date
+
+
+def get_jobs_wrapper(directory, status, pdf=None, verbose=False):
+    """
+    A wrapper for get_jobs: contains a few functions for tidyness
+    """
+    files = get_files(directory)
+    
+    pdf_new = pd.DataFrame()
+    if len(files)>0:
+        files = [directory+f for f in files]
+        pdf_new = get_jobs_df(files,verbose)
+        pdf_new['Status'] = status
+    
+    if (pdf is not None) & (pdf_new.empty == False):
+        return pd.concat([pdf, pdf_new])
+    elif pdf_new.empty:
+        return pdf
+    else:
+        return pdf_new
+
 
 def get_jobs_df(files, verbose=False):
     
@@ -85,64 +106,86 @@ def get_jobs_df(files, verbose=False):
 
     return pdf_jobs
 
-def update_master(pdf_new, verbose=False):
-
-    pdf_new_cols = pdf_new.columns.tolist()
-    if 'Open' not in pdf_new_cols:
-        pdf_new['Open'] = True
-        pdf_new_cols = ['Open'] + pdf_new_cols 
-        pdf_new  = pdf_new[pdf_new_cols]
-
+def get_compare_master(pdf_new):
+    
     # Cast the IDs
     pdf_new['Job ID'] = pdf_new['Job ID'].astype(str)    
+    # Measure the jobs per status
+    new_open = pdf_new[pdf_new['Status']=='3 Open']['Job ID'].count()
+    new_closed = pdf_new[pdf_new['Status']=='2 Closed']['Job ID'].count()
+    new_applied = pdf_new[pdf_new['Status']=='1 Applied']['Job ID'].count()
+    new_ignored = pdf_new[pdf_new['Status']=='0 Ignored']['Job ID'].count()  
 
+    # Open and cast the master
     pdf_master = pd.read_csv('master.csv')
     pdf_master['Job ID'] = pdf_master['Job ID'].astype(str)
+    # Measure the jobs per status in the master
+    master_open = pdf_master[pdf_master['Status']=='3 Open']['Job ID'].count()
+    master_closed = pdf_master[pdf_master['Status']=='2 Closed']['Job ID'].count()
+    master_applied = pdf_master[pdf_master['Status']=='1 Applied']['Job ID'].count()
+    master_ignored = pdf_master[pdf_master['Status']=='0 Ignored']['Job ID'].count()    
 
     # Merge on the Job IDs
-    pdf_merge = pdf_master[['Job ID', 'Open']].merge(pdf_new[['Job ID', 'Open']], on='Job ID', how='outer', suffixes = ['_old',''])
+    pdf_merge = pdf_master[['Job ID', 'Status']].merge(pdf_new[['Job ID', 'Status']], on='Job ID', how='outer', suffixes = ['_old',''])
     
-    # Fill the 'Open' NANs with False since they are closed
-    pdf_merge['Open'].fillna(value = False, inplace=True)
+    # Fill the 'Status' NANs with False since they are closed
+    added = pdf_merge[pdf_merge['Status_old'].isna()]['Job ID'].count() 
     
-    added = pdf_merge[pdf_merge['Open_old'].isna()]['Job ID'].count()
-    open_ = pdf_merge[pdf_merge['Open']==True]['Job ID'].count()
-    closed = pdf_merge[pdf_merge['Open']==False]['Job ID'].count()
-     
-    if verbose:
-        print(f'Newly added {added}\nTotal open {open_}\nTotal closed {closed}')
+    print(f'Newly added {added}')
         
     # Get the new Job IDs
-    additional_ids = list(set(pdf_new['Job ID'].tolist()).difference(set(pdf_master['Job ID'].tolist())))
+    pdf_master_count = pdf_master.groupby('Status').agg({'Job ID':'count'}).reset_index().rename(columns={'Job ID':'counts_master'})
+    pdf_new_count = pdf_new.groupby('Status').agg({'Job ID':'count'}).reset_index().rename(columns={'Job ID':'counts_new'})
 
-    # Set up the cols and conct the DFs
-    pdf_master  = pdf_master[pdf_new_cols]
-    pdf_new_master = pd.concat([pdf_master, pdf_new[pdf_new['Job ID'].isin(additional_ids)]],sort=False)
+    # Get the difference in counts 
+    # TODO: update this to show what changes to what
+    pdf_merge = pdf_new_count.merge(pdf_master_count, on='Status', how='outer')
+    pdf_merge.fillna(value = 0, inplace=True)
+    pdf_merge = pdf_merge.astype({"counts_new": int, "counts_master": int})
+    
+    pdf_merge['Status'] = pdf_merge['Status'].apply(lambda s: s.split(' ')[1]) 
+    
+    print(pdf_merge)
+    
+    return None
 
-    # Remove and replace the 'Open' column
-    pdf_new_master = pdf_new_master.drop(columns=['Open'])
-    pdf_new_master = pdf_new_master.merge(pdf_merge[['Job ID', 'Open']], on='Job ID', how='inner')
 
-    # De-duplicate the new master DB
-    pdf_new_master.drop_duplicates(inplace=True)
+def update_master(pdf, master_filename=None, verbose=False):
 
+    pdf_jobs_cols = [ 'Status',
+        'pdate', 'Job ID', 'Job title', 'Company Name', 'DE', 'Company Location',
+        'Seniority Level', 'Industry', 'Employment Type', 'Job Functions', 
+        'Number of applicants', 'pdatetime', 'Posted Date', 'cdatetime',
+        'cdate'
+        ]
+    
     # Resort the new master
-    pdf_new_master = pdf_new_master.sort_values(['Open','DE','pdate','Company Name'], ascending=[False,True,True,True]).reset_index(drop=True)
-    pdf_new_master  = pdf_new_master[pdf_new_cols]
+    pdf = pdf.sort_values(['Status','DE','pdate','Company Name'], ascending=[False,True,True,True]).reset_index(drop=True)
+
+    pdf = pdf[pdf_jobs_cols]
+    
+    # Write to the master DB
+    if master_filename:
+        pdf.to_csv(master_filename, index=False)
+    else:
+        pdf.to_csv('master.csv',index=False)
+    
+    return None
 
 
-    return pdf_new_master
-
-
-def main(args_list, length):
+def main(directory, master_filename, verbose):
     
     # Get the inputs
+    '''
     verbose = False
-    if length > 2:
+    if length > 3:
         verbose = True
-    
-    directory = args_list[1]
-    
+        update_master = True
+    elif length > 2:
+        verbose = True
+        update_master = False
+    '''
+        
     # Get the files to process
     files = get_files(directory)
     
@@ -150,18 +193,54 @@ def main(args_list, length):
     rename_files_and_dirs(files, directory, verbose)
 
     # Process and create the DataFrame
-    files = [directory+f for f in files]
-    pdf_new = get_jobs_df(files)
+    pdf_new = get_jobs_wrapper(directory,'3 Open', None, verbose)
+
+    closed_dir = directory+'Closed/'
+    pdf_new = get_jobs_wrapper(closed_dir,'2 Closed', pdf_new, verbose)
+        
+    applied_dir = directory+'Applied/'
+    pdf_new = get_jobs_wrapper(applied_dir,'1 Applied', pdf_new, verbose)
+                             
+    ignored_dir = directory+'Ignored/'
+    pdf_new = get_jobs_wrapper(ignored_dir,'0 Ignored', pdf_new, verbose)       
+    
+    #if verbose: 
+    get_compare_master(pdf_new)
     
     # Update the master DB
-    pdf_out = update_master(pdf_new, verbose)
-    
-    # Write to the master DB
-    pdf_out.to_csv('master.csv',index=False)
+    if master_filename:
+        update_master(pdf_new, master_filename, verbose)
     
     return None
 
 
 if __name__ == '__main__':
     
-    main(sys.argv, len(sys.argv))
+    # Create the parser
+    parser = argparse.ArgumentParser(prog='loli_scraping',
+                                        usage='%(prog)s [options] path',
+                                        description='List the content of a folder')
+    #parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+
+    parser.add_argument('-d',
+                        '--directory',
+                        help='Directory where the html files are stored',
+                        type=str, required=True)
+
+    parser.add_argument('-m',
+                        '--master',
+                        help='Name of master db (will overwrite)',
+                        type=str)
+
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='store_true',                        
+                        help='Write processing info to stdio')
+
+
+    # Execute parse_args()
+    args = parser.parse_args()
+
+    main(args.directory, args.master, args.verbose)
+    
+    
